fe44e55a8344ffec1ac0b5a495653a735e682bf0
unknown <lancegulay@gmail.com>
2022-12-19 11:10:54 +0800

    pychat

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -1,10 +1,19 @@
 import openai
+import os
+
+OPENAI_API_KEY="OPENAI_API_KEY"
 
 def main():
+    openai_api_key = os.getenv(OPENAI_API_KEY)
+
+    if openai_api_key == None:
+        print('OPENAI_API_KEY required.')
+        exit(-1)
+
     print('PyChat v0.1') 
     #set OPENAI_API_KEY=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
     #git@github.com:LanceCMG/pychat.git
-    
+
     query = input('Input: ')
 
     completion = openai.Completion.create(engine='text-davinci-003', prompt=query, max_tokens=4000)
